9c177d3c630b45ed0407bc5be587c285ba57ebde
Cid <cidibeee@gmail.com>
2025-06-17 16:52:40 +0200

    Update Website: z.B. config angepasst

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://cidibeee.github.io/darkwater-syndicate/',
-	base: '/darkwater-syndicate/',
+	base: '/Darkwater_Website/',
 	integrations: [mdx(), sitemap()],
 });
