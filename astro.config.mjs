aaa13c648a8dacca1c509a35379be6545004f397
Cid <cidibeee@gmail.com>
2025-06-17 16:33:47 +0200

    Add deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://cidibeee.github.io/darkwater-syndicate/',
+	base: '/darkwater-syndicate/',
 	integrations: [mdx(), sitemap()],
 });
