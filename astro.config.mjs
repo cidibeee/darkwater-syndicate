cc38bf35779102d3d4213300b8cb3390e7b9d3b8
Cid <cidibeee@gmail.com>
2025-06-22 10:47:30 +0200

    Verlinkung fixen

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://cidibeee.github.io/darkwater-syndicate/',
-	base: '/darkwater-syndicate/',
+	site: 'https://darkwater-syndicate.de',
+	base: '/',
 	integrations: [mdx(), sitemap()],
 });
