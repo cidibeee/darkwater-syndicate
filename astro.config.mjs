78690d712f5ced9533a6e906258f0ffe55091a33
Cid <cidibeee@gmail.com>
2026-05-19 17:36:10 +0200

    Add legal pages and update footer links; configure Netlify integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-//import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://darkwater-syndicate.de',
 	base: '/',
 	integrations: [mdx(), sitemap()],
-	//adapter: netlify(),
+	adapter: netlify(),
 });
